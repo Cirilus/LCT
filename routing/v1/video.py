7855b4a0e382877c69ba76abe1ad8148acdedce8
kirill <shkolenko02-03-2004@mail.ru>
2023-11-04 12:52:07 +0300

    Init

---
+++ routing/v1/video.py
@@ -0,0 +1,33 @@
+import io
+import os
+from typing import List
+
+from fastapi import APIRouter, WebSocket, UploadFile, File
+from fastapi.responses import StreamingResponse
+
+router = APIRouter(prefix="/api/v1/ml", tags=["company"])
+
+
+@router.post(
+    "/binary",
+    description="get binary video and returning stream"
+)
+async def get_binary_video(video: UploadFile = File(...)):
+    video_stream = await video.read()
+
+    return StreamingResponse(io.BytesIO(video_stream), media_type="video/mp4")
+
+
+@router.websocket(
+    "/socket"
+)
+async def get_binary_video(websocket: WebSocket):
+    await websocket.accept()
+
+    while True:
+        data = await websocket.receive()
+        if isinstance(data, bytes):
+            await websocket.send_bytes(data)
+        else:
+            break
+
