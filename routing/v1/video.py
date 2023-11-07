3c6b71707af77075df3029073a09d01ef6d1963b
kirill <shkolenko02-03-2004@mail.ru>
2023-11-07 19:11:22 +0300

    Add streaming

--- routing/v1/video.py
+++ routing/v1/video.py
@@ -1,9 +1,11 @@
 import io
 import os
 from typing import List
-
-from fastapi import APIRouter, WebSocket, UploadFile, File
+import cv2
+from fastapi import APIRouter, WebSocket, UploadFile, File, HTTPException, status
 from fastapi.responses import StreamingResponse
+
+
 
 router = APIRouter(prefix="/api/v1/ml", tags=["company"])
 
@@ -19,15 +21,27 @@ async def get_binary_video(video: UploadFile = File(...)):
 
 
 @router.websocket(
-    "/socket"
+    "/rl"
 )
 async def get_stream_video(websocket: WebSocket):
+    # rtsp_url = "rtsp://admin:A1234567@188.170.176.190:8027/Streaming/Channels/101"
     await websocket.accept()
 
+    rtsp_url = await websocket.receive_text()
+
+    # rtsp_url = "rtsp://admin:A1234567@188.170.176.190:8027/Streaming/Channels/101?transportmode=unicast&profile=Profile_1"
+
+    cap = cv2.VideoCapture(rtsp_url)
+
+    if not cap.isOpened():
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="cannot open the rcp url")
+
     while True:
-        data = await websocket.receive()
-        if isinstance(data, bytes):
-            await websocket.send_bytes(data)
-        else:
+        ret, frame = cap.read()
+        if not ret:
             break
 
+        ret, buffer = cv2.imencode('.jpg', frame)
+        jpg_as_bytes = io.BytesIO(buffer.tobytes())
+        await websocket.send_bytes(jpg_as_bytes.getvalue())
+        await websocket.receive_text()
